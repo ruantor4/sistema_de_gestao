d88cd5a3ff78a82035ecfd2c1722e69f3ac1225d
ruantor4 <ruan.tor4@hotmail.com>
2025-10-30 00:29:57 -0300

    .env.exemple, requeriments.txt e ajustes de log no core

--- core/views.py
+++ core/views.py
@@ -1,8 +1,5 @@
-import traceback
-
 from django.contrib import messages
 from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.models import AbstractUser
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
@@ -11,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from core.utils import registrar_log
 
 
-class HomeView(LoginRequiredMixin, View): #
+class HomeView(LoginRequiredMixin, View):  #
     """
         Exibe a página inicial do sistema.
 
@@ -38,9 +35,10 @@ class HomeView(LoginRequiredMixin, View): #
             return render(request, 'core/home.html')
 
         except Exception as e:
-            registrar_log(request.user,"Acessar Home", "ERROR", str(e))
+            registrar_log(request.user, "Acessar Home", "ERROR", f"Erro ao carregar pagina: {str(e)}")
             messages.error(request, "Erro ao carregar a página inicial do sistema.")
             return redirect('login')
+
 
 class LoginView(View):
     """
@@ -50,6 +48,7 @@ class LoginView(View):
             get: Exibe o formulário de login.
             post: Processa as credenciais e autentica o usuário.
     """
+
     def get(self, request: HttpRequest) -> HttpResponse:
         """
             Exibe a página de login.
@@ -68,12 +67,11 @@ class LoginView(View):
             return render(request, 'core/login.html')
 
         except Exception as e:
-            registrar_log(request.user,"Acessar Login", "ERROR", str(e))
+            registrar_log(request.user, "Acessar Login", "ERROR", f"Erro inesperado:{str(e)}")
             messages.error(request, "Erro ao carregar a página de login.")
             return redirect('login')
 
-
-    def post(self, request:HttpRequest) -> HttpResponse:
+    def post(self, request: HttpRequest) -> HttpResponse:
         """
             Processa a autenticação do usuário.
 
@@ -95,15 +93,14 @@ class LoginView(View):
             usuario = authenticate(request, username=username, password=password)
             if usuario is not None:
                 login(request, usuario)
-                registrar_log(usuario, "Login", "SUCCESS", f'{username} fez login com sucesso!.')
                 return redirect('home')
             else:
                 messages.error(request, "Usuário ou senha incorretos.")
                 return render(request, 'core/login.html')
 
         except Exception as e:
-            registrar_log(request.user,"Login", "ERROR", str(e))
-            messages.error(request, "Erro inesperado ao processar o login." )
+            registrar_log(request.user, "Login", "ERROR", f"Erro inesperado: {str(e)}")
+            messages.error(request, "Erro inesperado ao processar o login.")
             return redirect('login')
 
 
@@ -113,6 +110,7 @@ class LogoutView(View):
 
         Finaliza a sessão ativa do usuário e o redireciona para a página de login.
     """
+
     def get(self, request: HttpRequest) -> HttpResponse:
         """
             Encerra a sessão do usuário autenticado.
@@ -127,13 +125,11 @@ class LogoutView(View):
             username = request.user.username if request.user.is_authenticated else 'Usuário desconhecido'
             if request.user.is_authenticated:
                 logout(request)
-                registrar_log(request.user, "Logout", "SUCCESS",
-                              f'{username} fez logout com sucesso!.')
-
+                messages.error(request, f'{username} fez logout com sucesso!.')
             return redirect('login')
 
         except Exception as e:
-            registrar_log(request.user,"Logout", "ERROR", str(e))
+            registrar_log(request.user, "Logout", "ERROR", f"Erro inesperado: {str(e)}")
             messages.error(request, "Erro inesperado ao encerrar a sessão.")
             return redirect('login')
 
@@ -145,6 +141,7 @@ class Erro404View(View):
         Métodos:
             get: Captura requisições para URLs não existentes, registra log e exibe a página de erro.
     """
+
     def get(self, request: HttpRequest, exception=None) -> HttpResponse:
         """
             Exibe a página de erro 404 quando uma URL não é encontrada.
@@ -158,14 +155,15 @@ class Erro404View(View):
         """
         try:
             registrar_log(
-                request.user if request.user.is_authenticated else None,"Erro Global", "WARNING",
-                                    f"404 - URL Não encontrada: {request.path}"
+                request.user if request.user.is_authenticated else None, "Erro Global", "WARNING",
+                f"404 - URL Não encontrada: {request.path}"
 
             )
             messages.warning(request, "Página não encontrada.")
             return render(request, 'core/404.html', status=404)
 
         except Exception as e:
+            registrar_log(request.user,"ERRO404", "ERROR", f"Erro inesperado: {str(e)}")
             return render(request, 'core/404.html', status=404)
 
 
@@ -176,7 +174,8 @@ class Erro500View(View):
         Métodos:
             get: Captura exceções inesperadas, registra log detalhado e exibe a página de erro.
     """
-    def get(self, request: HttpRequest, exception=None) -> HttpResponse:
+
+    def get(self, request: HttpRequest) -> HttpResponse:
         """
             Exibe a página de erro 500 quando ocorre um erro interno no servidor.
 
@@ -190,11 +189,11 @@ class Erro500View(View):
         try:
             registrar_log(
                 request.user if request.user.is_authenticated else None, "Erro Global", "WARNING",
-                                f"500 - Erro interno do servidor em {request.path}erro_detalhes"
+                f"500 - Erro interno do servidor em {request.path}erro_detalhes"
             )
-
             messages.error(request, "Ocorreu um erro inesperado. Entre em contato com o suporte.")
             return render(request, 'core/500.html', status=500)
 
         except Exception as e:
+            registrar_log(request.user, "ERRO500", "ERROR", f"Erro inesperado: {str(e)}")
             return render(request, 'core/500.html', status=500)
